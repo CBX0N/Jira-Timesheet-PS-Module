54773451eb96729b2795367c1c4dcd1acb0a0368
Charlie Boon <charlie.boon@sky.com>
2022-10-27 21:49:47 +0100

    Added  error message

--- Functions/Install-JiraTimesheetModule.psm1
+++ Functions/Install-JiraTimesheetModule.psm1
@@ -1,13 +1,16 @@
-Function Add-Install-JiraTimesheetModule {
-<#
-.SYNOPSIS
-.DESCRIPTION
-#>
-param(
-     )
-    $ModuleVersion = Get-Content -Path "..\*.psd1" | Select-String -SimpleMatch "ModuleVersion"
+Function Install-JiraTimesheetModule {
+
+    if(((test-path Jira.Timesheet.Powershell.psd1) -and (Test-Path ..\Jira.Timesheet.Powershell.psd1)) -eq $False){
+        Write-Error -Message "Cannot find Module Manifest." -Category ObjectNotFound
+    }
+
+    if((Test-Path ..\Jira.Timesheet.Powershell.psd1) -eq $True) {
+        Set-location ..\
+    }
+
+    $ModuleVersion = Get-Content -Path "*.psd1" | Select-String -SimpleMatch "ModuleVersion"
     $ModuleVersion = (($ModuleVersion -split '=' | Select-Object -last 1) -replace " ","") -replace "'",''
-    $ModuleName    = Get-Content -Path "..\*.psd1" | Select-String -SimpleMatch "RootModule"
+    $ModuleName    = Get-Content -Path "*.psd1" | Select-String -SimpleMatch "RootModule"
     $ModuleName    = ((($ModuleName -split '=' | Select-Object -last 1) -replace " ","") -replace "'",'') -replace ".psm1",''
     $PSModulePath  = $env:PSModulePath -split ";" | Select-Object -First 1
 
@@ -15,10 +18,11 @@ param(
     Write-Output "Creating Directory:::: $PSModulePath\$ModuleName\$ModuleVersion"
     New-Item -Path $PSModulePath\$ModuleName -Name $ModuleVersion -ItemType Directory | Out-Null 
 
-    Get-ChildItem -Path '..\' -Recurse | ForEach-Object {
+    Get-ChildItem -Recurse | ForEach-Object {
         Write-Output "Copying:::: $_ to Module Folder"
         }
 
-    Copy-Item -path '..\*' -Destination $PSModulePath\$ModuleName\$ModuleVersion -Recurse | Out-Null 
+    Copy-Item -path '*' -Destination $PSModulePath\$ModuleName\$ModuleVersion -Recurse | Out-Null
+
 }
 Export-ModuleMember -Function Install-JiraTimesheetModule
