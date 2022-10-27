e3acbd7ce4d5358f3d7b230a68442331f0db8c52
Charlie Boon <charlie.boon@sky.com>
2022-10-27 22:06:39 +0100

    Clean Up

--- Install-JiraTimesheetModule.ps1
+++ Install-JiraTimesheetModule.ps1
@@ -1,7 +1,7 @@
 Function Install-JiraTimesheetModule {
 
     if(((test-path Jira.Timesheet.Powershell.psd1) -eq $False ) -and ((Test-Path ..\Jira.Timesheet.Powershell.psd1) -eq $False ) ){
-        Write-Error -Message "Cannot find Module Manifest." -Category ObjectNotFound
+        throw "Cannot find Module Manifest."
     }
 
     if((Test-Path ..\Jira.Timesheet.Powershell.psd1) -eq $True) {
@@ -17,12 +17,11 @@ Function Install-JiraTimesheetModule {
     Write-Output "Module Parameters:::: ModuleName: $ModuleName ModuleVersion: $ModuleVersion" 
     Write-Output "Creating Directory:::: $PSModulePath\$ModuleName\$ModuleVersion"
     New-Item -Path $PSModulePath\$ModuleName -Name $ModuleVersion -ItemType Directory | Out-Null 
-
-    Get-ChildItem -Recurse | ForEach-Object {
-        Write-Output "Copying:::: $_ to Module Folder"
-        }
-
+    
+    Write-Output "Copying:::: Files to Module Folder"
     Copy-Item -path '*' -Destination $PSModulePath\$ModuleName\$ModuleVersion -Recurse | Out-Null
-
+    
+    Write-Output "Module Installed"
 }
-Export-ModuleMember -Function Install-JiraTimesheetModule
+
+ Install-JiraTimesheetModule
--- Jira.Timesheet.Powershell.psd1
+++ Jira.Timesheet.Powershell.psd1
@@ -1,13 +1,13 @@
 @{
 	RootModule 		        = 'Jira.Timesheet.Powershell.psm1' 
-	ModuleVersion 		    = '1.0.3' 
+	ModuleVersion 		    = '1.0.7' 
 	CompatiblePSEditions 	= 'Desktop', 'Core' 
 	GUID 			        = '0a8b320f-8c4e-475f-873d-2b660c03905c' 
 	Author 			        = 'Charlie Boon' 
 	CompanyName 		    = 'CBXON Limited' 
 	Copyright 		        = '(c) CBXON Limited. All rights reserved.' 
 	Description 		    = 'Jira.Timesheet.Powershell'
-	FunctionsToExport 	    = 'Add-TimeToJira','New-JiraTimesheet','Install-JiraTimesheetModule'
+	FunctionsToExport 	    = 'Add-TimeToJira','New-JiraTimesheet'
 	CmdletsToExport 	    = @() 
 	VariablesToExport 	    = @() 
 	AliasesToExport 	    = @() 
